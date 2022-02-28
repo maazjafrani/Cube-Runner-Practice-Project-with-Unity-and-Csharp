ae711c5e8dce23f091204508151db5c65a9ee592
maazjafrani <80782814+maazjafrani@users.noreply.github.com>
2022-02-28 16:09:48 +0200

    Starting game when the user taps on screen and also showing the final score at the end

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;//since we are working with UI element
 public class Score : MonoBehaviour
 {
     public Text scoreText;// creating a variable scoreText to get the text field.
-    int myScore=0;   
+    int myScore=0;
+
+    public Text finalScoreText;  //willget the final score
    
     // Update is called once per frame
     void Update()
     {
         scoreText.text = myScore.ToString();  //converting int to String and then storing it to the score text container.
+        finalScoreText.text = "Score: " +myScore.ToString();// also storing the score into finalScoreText variable and showing it once game is over
         
     }
     public void AddScore(int score)  //method to increment myScore variable 
